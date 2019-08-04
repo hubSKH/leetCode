2f8dd97e406c2a91fe45801df576a820ecf501bb
SHENKAIHUAN <749151125@qq.com>
2019-08-04 18:30:12 +0800

    215. 数组中的第K个最大元素（快排思路）

--- com/skh/leetcode/array/KthLargestElementInAnArray.java
+++ com/skh/leetcode/array/KthLargestElementInAnArray.java
@@ -1,8 +1,9 @@
 package leetcode.array;
 
-import java.util.Arrays;
+import java.util.Random;
 
 /**
+ * 215. 数组中的第K个最大元素
  * @Description
  * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
  *
@@ -16,8 +17,8 @@ import java.util.Arrays;
  */
 public class KthLargestElementInAnArray {
     public static void main(String[] args) {
-        int[] nums = new int[]{1};
-        int k=1;
+        int[] nums = new int[]{5,6,4,3,2,1};
+        int k=2;
         for (int i: nums){
             System.out.print (i+",");
         }
@@ -25,31 +26,80 @@ public class KthLargestElementInAnArray {
         long start =System.currentTimeMillis ();
         System.out.println (findKthLargest(nums,k));
         long end =System.currentTimeMillis ();
+        System.out.println ("===============after==============");
+        for (int i: nums){
+            System.out.print (i+",");
+        }
+        System.out.println ();
         System.out.println ("total:"+(end-start));
     }
+//    public static int findKthLargest(int[] nums, int k) {
+//        Arrays.sort (nums);
+//        return nums[nums.length-k];
+//    }
+
+    /**
+     * 快速排序思路
+     * @param nums
+     * @param k
+     * @return
+     */
     public static int findKthLargest(int[] nums, int k) {
-        Arrays.sort (nums);
-        return nums[nums.length-k];
+        System.out.println (quickSelect(nums,0,nums.length-1,nums.length-k));
+        return quickSelect(nums,0,nums.length-1,nums.length-k);
     }
 
-//    /**
-//     * 快速排序思路
-//     * @param nums
-//     * @param k
-//     * @return
-//     */
-//    public static int findKthLargest(int[] nums, int k) {
-//        int temp=nums[0];
-//        for(int i=0;i<k;i++){
-//            for(int j=i;j<nums.length;j++){
-//                if(nums[j]>nums[i]){
-//                    temp = nums[j];
-//                    nums[j] = nums[i];
-//                    nums[i] = temp;
-//                }
-//            }
-//        }
-//        return nums[k-1];
-//    }
+    private static int quickSelect(int[] nums, int low, int hight, int pos) {
+
+        if(low == hight){
+            return nums[low];
+        }
+
+        int i=low;
+        int j=hight;
+        int pov_idx = position(nums,low,hight,low+new Random ().nextInt(hight - low));
+        if(pov_idx == pos){
+            return nums[pov_idx];
+        }else if(pov_idx > pos){
+            return quickSelect (nums,low,pov_idx-1,pos);
+        }else {
+            return quickSelect (nums,pov_idx+1,hight,pos);
+        }
+    }
+
+    /**
+     *
+     * @param nums
+     * @param low
+     * @param hight
+     * @param pos
+     * @return
+     */
+    private static int position(int[] nums, int low, int hight, int pos) {
+
+        int num =nums[pos];
+        int store_idx = low;
+        swap(nums,pos, hight);
+        for(int i=low;i<=hight;i++){
+            if(nums[i]<num){
+                swap (nums,store_idx,i);
+                store_idx++;
+            }
+        }
+        swap (nums,store_idx,hight);
+        return store_idx;
+    }
+
+    /**
+     * 交换
+     * @param nums
+     * @param i
+     * @param j
+     */
+    private static void swap (int[] nums ,int i ,int j ){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
 }
