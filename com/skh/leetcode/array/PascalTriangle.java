99eca179d23c51aa3650890bf806d8c9a0e2ec94
SHENKAIHUAN <749151125@qq.com>
2019-08-13 23:28:30 +0800

    118. 杨辉三角

---
+++ com/skh/leetcode/array/PascalTriangle.java
@@ -0,0 +1,59 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 118. 杨辉三角
+ * @Description
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ * @auther SHENKAIHUAN
+ * @create 2019-08-13 22:53
+ */
+public class PascalTriangle {
+    public static void main(String[] args) {
+        int num = 5;
+        long start = System.currentTimeMillis ();
+        List<List<Integer>> result =generate(num);
+        long end =System.currentTimeMillis ();
+        System.out.println ("total:"+(end-start));
+        for(List<Integer> list : result){
+            for(Integer integer :list){
+                System.out.print(integer+", ");
+            }
+            System.out.println ();
+        }
+    }
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> lists = new ArrayList<> ();
+        List<Integer> lastList=null;
+        if(numRows<1){
+            return lists;
+        }
+        for(int i=1;i<=numRows;i++){
+            if(i>1){
+                lastList= lists.get (i-2);
+            }
+            List<Integer> integers = new ArrayList<> ();
+            for(int j=0;j<i;j++){
+                if(j==0 || j==i-1){
+                    integers.add (1);
+                }else {
+                    int num = lastList.get (j-1)+lastList.get (j);
+                    integers.add (num);
+                }
+            }
+            lists.add (integers);
+        }
+        return lists;
+    }
+}
