30a66b3f0a611381d9ff6574a290f128876f7cbe
SHENKAIHUAN <749151125@qq.com>
2020-05-01 22:30:49 +0800

    面试题35. 复杂链表的复制

---
+++ com/skh/assit/Node.java
@@ -0,0 +1,18 @@
+package assit;
+
+/**
+ * @Description
+ * @auther SHENKAIHUAN
+ * @create 2020-05-01 20:01
+ */
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
