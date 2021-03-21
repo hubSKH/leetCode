4554936e8a6914e29430bbe32c5a1914952d2b5b
SHENKAIHUAN <749151125@qq.com>
2021-03-21 16:41:01 +0800

    assit

--- com/skh/assit/ListNode.java
+++ com/skh/assit/ListNode.java
@@ -7,4 +7,14 @@ public class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return val+"->"+next ;
+    }
 }
