2bf1b555f1427e663e74cb9504da6574e6543c9e
SHENKAIHUAN <749151125@qq.com>
2021-10-11 08:17:04 +0800

    695. 岛屿的最大面积

---
+++ com/skh/leetcode/array/MaxAreaOfIsland.java
@@ -0,0 +1,63 @@
+package leetcode.array;
+
+/**
+ * 695. 岛屿的最大面积
+ *
+ * @Description 给你一个大小为 m x n 的二进制矩阵 grid 。
+ * 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。
+ * 你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ * 岛屿的面积是岛上值为 1 的单元格的数目。
+ * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+ * <p>
+ * 输入：grid = [
+ * [0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 输出：6
+ * @auther SHENKAIHUAN
+ * @create 2021-10-10 16:42
+ */
+public class MaxAreaOfIsland {
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        System.out.println (new MaxAreaOfIsland ().maxAreaOfIsland (grid));
+    }
+
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int row = grid.length, col = grid[0].length;
+        int max = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    int count = dfs (grid, i, j);
+                    max = Math.max (max, count);
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1 || grid[i][j] == 0) {
+            return 0;
+        }
+        int count = 1;
+        grid[i][j] = 0;
+        count += dfs (grid, i - 1, j);
+        count += dfs (grid, i + 1, j);
+        count += dfs (grid, i, j - 1);
+        count += dfs (grid, i, j + 1);
+        return count;
+    }
+}
