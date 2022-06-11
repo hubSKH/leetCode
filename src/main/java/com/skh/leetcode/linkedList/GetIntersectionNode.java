2a0d02780877611d5454db1c98aa07d822e26707
SHENKAIHUAN <749151125@qq.com>
2022-06-11 17:15:24 +0800

    160. 相交链表

---
+++ src/main/java/com/skh/leetcode/linkedList/GetIntersectionNode.java
@@ -0,0 +1,63 @@
+package leetcode.linkedList;
+
+import assit.ListNode;
+
+/**
+ * 160. 相交链表
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ * <p>
+ * 自定义评测：
+ * <p>
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+ * <p>
+ * 示例 1：
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * <p>
+ * 提示：
+ * listA 中节点数目为 m
+ * listB 中节点数目为 n
+ * 1 <= m, n <= 3 * 104
+ * 1 <= Node.val <= 105
+ * 0 <= skipA <= m
+ * 0 <= skipB <= n
+ * 如果 listA 和 listB 没有交点，intersectVal 为 0
+ * 如果 listA 和 listB 有交点，intersectVal == listA[skipA] == listB[skipB]
+ *
+ * @auther SHENKAIHUAN
+ * @create 2022/6/11 16:43
+ */
+public class GetIntersectionNode {
+    public static void main(String[] args) {
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = new ListNode(8);
+        headA.next.next.next = new ListNode(4);
+        headA.next.next.next.next = new ListNode(5);
+
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = headA.next.next;
+        ListNode intersectionNode = new GetIntersectionNode().getIntersectionNode(headA, headB);
+        System.out.println(intersectionNode);
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = headA, q = headB;
+         while (p != q) {
+            p = p == null ? headB : p.next;
+             q = q == null ? headA : q.next;
+        }
+        return p;
+    }
+}
